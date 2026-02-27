085e320ef2d46d1f2bddf18a42aa1c3eb6d5ce4b
jaisonamith16-collab <jaisonamith16@gmail.com>
2026-02-27 09:42:17 +0530

    UC1: Add welcome message module

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,2 +1,13 @@
 public class PalindromeCheckerApp {
+    public static void main(String[] args) {
+
+        System.out.println("=======================================");
+        System.out.println("      Welcome to PalindromeChecker     ");
+        System.out.println("=======================================");
+
+        System.out.println("Application Name : PalindromeChecker App");
+        System.out.println("Version          : 1.0.0");
+
+        System.out.println("Application started successfully.");
+    }
 }
